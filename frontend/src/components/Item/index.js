53a344683b4bda57a7961467108cf1bc09f0770b
WilcoApp <hello@wilco.gg>
2023-02-05 08:34:20 +0000

    Search party

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -8,7 +8,6 @@ import {
   ITEM_PAGE_UNLOADED,
 } from "../../constants/actionTypes";
 import { getItemAndComments } from "./utils/ItemFetcher";
-import placeholderImg from "../../imgs/placeholder.png";
 
 const mapStateToProps = (state) => ({
   ...state.item,
@@ -49,7 +48,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image ? this.props.item.image : placeholderImg}
+                src={this.props.item.image}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
